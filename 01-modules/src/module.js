e6c947f07e6c09bea1377bbb90d2e622ae86cb4b
Vanessa Winklbauer <vwinklbauer.mmt-b2015@fh-salzburg.ac.at>
2016-11-10 13:25:09 +0100

    regex plus more tests

--- 01-modules/src/module.js
+++ 01-modules/src/module.js
@@ -1,15 +1,12 @@
-export function valid(email) {
-  makeArray(email);
+const regex = /^[a-zA-Z0-9._%+-]+\.([a-zA-Z]{3,4})-(b|m)(\d{4})@(fh-salzburg.ac.at)$/;
 
-  if (array[1].length!=3) return false; //degreeProgram 3 letters
-  if (!(array[2]=='m' || array[2]=='b')) return false; //level is m or b
-  if (array[3].length!=4 || isNaN(array[3])) return false; //year 4 letters or not number
-  if (array[4] != "@fh-salzburg.ac.at") return false;
-  return true;
+export function valid(email) {
+  return regex.test(email);
 }
 
 export function degreeProgram(email) {
   if (valid(email)) {
+    const array = regex.exec(email);
     return array[1].toUpperCase();
   }
   return false;
@@ -17,6 +14,7 @@ export function degreeProgram(email) {
 
 export function level(email) {
   if (valid(email)) {
+    const array = regex.exec(email);
     return array[2].toUpperCase()+"A";
   }
   return false;
@@ -24,24 +22,10 @@ export function level(email) {
 
 export function graduationYear(email) {
   if (valid(email)) {
+    const array = regex.exec(email);
     if (array[2]=='b')
       return parseInt(array[3],10)+3;
     else return parseInt(array[3],10)+2;
   }
   return false;
-}
-
-var array;
-
-function makeArray(email) {
-  array = []
-  array.push(email.split('.', 1)[0]);//name
-  email = email.substr(email.indexOf('.')+1, email.length-1);
-  array.push(email.split('-', 1)[0]);//degreeProgram
-  email = email.substr(email.indexOf('-')+1, email.length-1);
-  var levelYear = email.split('@', 1)[0]; //level and year
-  array.push(levelYear[0]); //level
-  array.push(levelYear.substr(1, levelYear.length-1)); //year
-  email =  email.substr(email.indexOf('@'), email.length-1);
-  array.push(email);//rest
 }
--- 01-modules/test/index.js
+++ 01-modules/test/index.js
@@ -14,11 +14,32 @@ describe('01-modules', () => {
     it('should be an invalid FH/MMT email', () => {
       valid('hannes.moser@fh-salzburg.ac.at').should.be.false()
     })
+    it('should be an invalid FH/MMT email', () => {
+      valid('hmosermmt-b2015@fh-salzburg.ac.at').should.be.false()
+    })
+    it('should be an invalid FH/MMT email', () => {
+      valid('hmoser.mm-b2015@fh-salzburg.ac.at').should.be.false()
+    })
+    it('should be an invalid FH/MMT email', () => {
+      valid('hmoser.mmt-b20a5@fh-salzburg.ac.at').should.be.false()
+    })
+    it('should be an invalid FH/MMT email', () => {
+      valid('hmoser.mmt-b2015@fh-salzburg.acat').should.be.false()
+    })
   })
 
   describe('#degreeProgram()', () => {
     it('should match MMT', () => {
       degreeProgram('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('MMT')
+    })
+    it('should match MMA', () => {
+      degreeProgram('hmoser.mma-b2015@fh-salzburg.ac.at').should.eql('MMA')
+    })
+    it('should match ITSB', () => {
+      degreeProgram('hmoser.itsb-b2015@fh-salzburg.ac.at').should.eql('ITSB')
+    })
+    it('should be a invalid FH/MMT email', () => {
+      degreeProgram('hmosermmt-b2015@fh-salzburg.ac.at').should.be.false()
     })
   })
 
@@ -26,11 +47,23 @@ describe('01-modules', () => {
     it('should match BA or MA', () => {
       level('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql('BA')
     })
+    it('should match BA or MA', () => {
+      level('hmoser.mmt-m2015@fh-salzburg.ac.at').should.eql('MA')
+    })
+    it('should be a invalid FH/MMT email', () => {
+      level('hmosermmt-b2015@fh-salzburg.ac.at').should.be.false()
+    })
   })
 
   describe('#graduationYear()', () => {
     it('should be your graduation year', () => {
       graduationYear('hmoser.mmt-b2015@fh-salzburg.ac.at').should.eql(2018)
+    })
+    it('should be your graduation year', () => {
+      graduationYear('hmoser.mmt-m2015@fh-salzburg.ac.at').should.eql(2017)
+    })
+    it('should be a invalid FH/MMT email', () => {
+      graduationYear('hmosermmt-b2015@fh-salzburg.ac.at').should.be.false()
     })
   })
 })
