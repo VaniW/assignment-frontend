f50f14b00f8e21c91dade3c6eecf54d9eafd57ff
Vanessa Winklbauer <vwinklbauer.mmt-b2015@fh-salzburg.ac.at>
2016-11-15 22:13:20 +0100

    ue2 router

--- 02-router/src/app.js
+++ 02-router/src/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 import router from './router'
 import homeTpl from './templates/home.hbs'
 import contactTpl from './templates/contact.hbs'
+import playerTpl from './templates/player.hbs'
 import notFoundTpl from './templates/not-found.hbs'
 
 const $app = $('#app')
@@ -14,11 +15,18 @@ function contact() {
   $app.html(contactTpl())
 }
 
+function player(pname) {
+  $app.html(playerTpl({
+    pname
+}))
+}
+
 function notFound() {
   $app.html(notFoundTpl())
 }
 
 router('/', index)
 router('/contact', contact)
+router('/players/:player', player)
 router('*', notFound)
 router()
--- 02-router/src/router.js
+++ 02-router/src/router.js
@@ -1,3 +1,70 @@
-export default function() {
+import $ from 'jquery'
 
+const routes = new Map();
+
+export default function(route, fn) {
+  if (route && fn) {
+    routes.set(route, fn)
+    return
+  }
+
+  //init if no params
+  if (!route && !fn) {
+    init()
+    return
+  }
+}
+
+function init(){
+  eventHandler();
+  const path = window.location.pathname; //gets uri of current page
+  goto(path);
+}
+
+function eventHandler() {
+  window.addEventListener('popstate', e => {
+    if (history.state) {
+      let path = e.state.pathname;
+      goto(path, true);
+    }
+  });
+
+  const link = $('nav a');
+
+  for (var i = 0; i < link.length; i++) {
+    link[i].addEventListener('click', e => {
+      e.preventDefault();
+      goto(e.target.pathname);
+    });
+  }
+
+}
+
+const regex = /(\/players\/)([a-z0-9]+)/gi;
+
+function goto(route, init){
+  let uri= '';
+  let path = route;
+
+  if (route.match(regex)) {
+    const array = regex.exec(route);
+    let p = array[2];
+    uri = p;
+    path = '/players/:player';
+    route = '/players/'+uri;
+  }
+
+  if (!routes.has(path)) {
+    path = '*';
+  }
+
+  if (history.state && !init) {
+    let last = history.state.pathname;
+    if (last!=route) {
+      history.pushState({pathname: route}, '', route);
+    }
+  }
+  if (!init)  history.replaceState({pathname: route}, '', route);
+
+  return routes.get(path)(uri);
 }
