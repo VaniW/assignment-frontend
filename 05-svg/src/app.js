8cd0501d5dc1110f0d400d494563b6d00737c861
Vanessa Winklbauer <vwinklbauer.mmt-b2015@fh-salzburg.ac.at>
2017-01-08 22:05:08 +0100

    05ue

--- 05-svg/src/app.js
+++ 05-svg/src/app.js
@@ -31,6 +31,7 @@ function text() {
 
 function interaction() {
   app.innerHTML = interactionTpl()
+  addInteraction();
 }
 
 function notFound() {
@@ -44,3 +45,84 @@ router('/text', text)
 router('/interaction', interaction)
 router('*', notFound)
 router()
+
+function addInteraction() {
+
+  function SVGMenu( el, options ) {
+    this.el = el;
+    this.init();
+    console.log("init");
+  }
+
+  SVGMenu.prototype.init = function() {
+    this.trigger = this.el.querySelector( '#topLayer' );
+    this.initEvents();
+  };
+
+  SVGMenu.prototype.initEvents = function() {
+    this.trigger.addEventListener( 'mousedown', clicked );
+    this.trigger.addEventListener( 'mouseup', clickedend );
+    this.trigger.addEventListener( 'mouseenter', over );
+    this.trigger.addEventListener( 'mouseleave', clickedend );
+  };
+
+  if(document.getElementById( 'button' ) != null )
+    new SVGMenu(document.getElementById( 'button' ));
+
+  function over(e) {
+    console.log("over");
+    this.addEventListener('mousedown', clicked )
+  }
+
+  function leave(e) {
+    console.log("leave");
+    this.removeEventListener('mousemove', drag )
+    this.removeEventListener('mousedown', clicked )
+  }
+
+  function clickedend(e) {
+    console.log("clickedend");
+    this.removeEventListener('mousemove', drag )
+  }
+
+  let start;
+
+  function clicked(e) {
+
+    console.log("clicked");
+
+    var currentTransform = this.getAttribute('transform');
+    var regex  = /translate\(\s*([^\s,)]+)[ ,]([^\s,)]+)/.exec(currentTransform);
+    var currentPositionX = regex[1];
+
+    if(currentPositionX < 0){
+      start = e.clientX;
+      this.addEventListener('mousemove', drag);
+    }
+  };
+
+  function drag(e) {
+    console.log("dragged");
+    var currentTransform = this.getAttribute('transform');
+    var regex  = /translate\(\s*([^\s,)]+)[ ,]([^\s,)]+)/.exec(currentTransform);
+    var currentPositionX = regex[1];
+
+    let currentValue = parseInt(currentPositionX)
+    let newValue = currentValue+(e.clientX - start)
+
+
+    if(newValue > -285)
+      this.setAttribute("transform", 'translate('+newValue+',0)')
+
+    start = e.clientX;
+
+    if(newValue >= 1){
+      this.removeEventListener('mousemove', drag )
+      this.setAttribute("transform", 'translate(0,0)')
+      alert("committed");
+      location.reload();
+    }
+  }
+
+
+};
